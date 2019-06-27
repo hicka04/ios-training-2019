31afd1274964205a0540a51d3ddf91e7f64cb337
佐藤 光 <sato.hikaru@nifty.co.jp>
2019-06-27 18:44:14 +0900

    add project

---
+++ ios-training/ios-training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-training
+//
+//  Created by SCI01552 on 2019/06/27.
+//  Copyright © 2019 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
