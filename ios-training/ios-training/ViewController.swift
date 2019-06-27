92c0ee089a63a83be1c6bbf0e101089d08ca5257
hicka04 <hicka04@gmail.com>
2019-06-27 19:37:20 +0900

    てきとーに画面作って遊ぶ

--- ios-training/ios-training/ViewController.swift
+++ ios-training/ios-training/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label.text = "hogehoge"
     }
 
 
