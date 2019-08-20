176d043c50c2a2aea77bb70e8f3a38e38e4c99c0
hicka04 <hicka04@gmail.com>
2019-08-20 19:28:00 +0900

    Todo表示できる機能追加

---
+++ TODO/TODO/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  TODO
+//
+//  Created by SCI01552 on 2019/08/20.
+//  Copyright © 2019 hicka04. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    
+    let title: String
+}
--- TODO/TODO/TodoListViewController.swift
+++ TODO/TODO/TodoListViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class TodoListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var todoList: [Todo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "todoCell")
+        
+        tableView.dataSource = self
+        
+        todoList.append(Todo(title: "洗濯"))
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension TodoListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        cell.textLabel?.text = todoList[indexPath.row].title
+        return cell
     }
 }
