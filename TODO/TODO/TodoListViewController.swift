620b89ec2ccbfb98ec46f53366c6a2f12b5bf35f
hicka04 <hicka04@gmail.com>
2019-09-03 19:32:12 +0900

    TODOの完了と削除ができるよう変更

--- TODO/TODO/TodoListViewController.swift
+++ TODO/TODO/TodoListViewController.swift
@@ -12,11 +12,8 @@ class TodoListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var todoList: [Todo] = [] {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    var todoList: [Todo] = []
+    var doneList: [Todo] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +22,7 @@ class TodoListViewController: UIViewController {
                            forCellReuseIdentifier: "todoCell")
         
         tableView.dataSource = self
+        tableView.delegate = self
         
         let createButton = UIBarButtonItem(barButtonSystemItem: .add,
                                            target: self,
@@ -43,7 +41,7 @@ class TodoListViewController: UIViewController {
     }
 }
 
-extension TodoListViewController: UITableViewDataSource {
+extension TodoListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoList.count
@@ -54,11 +52,40 @@ extension TodoListViewController: UITableViewDataSource {
         cell.textLabel?.text = todoList[indexPath.row].title
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let doneButton = UITableViewRowAction(style: .normal, title: "Done") { _, _ in
+            tableView.beginUpdates()
+            self.doneList.append(self.todoList[indexPath.row])
+            self.todoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+        doneButton.backgroundColor = UIColor.blue
+        
+        let deleteButton = UITableViewRowAction(style: .destructive, title: "Delete") { _, _ in
+            tableView.beginUpdates()
+            self.todoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+        deleteButton.backgroundColor = UIColor.red
+        
+        return [doneButton, deleteButton]
+    }
 }
 
 extension TodoListViewController: CreateTodoViewControllerDelegate {
     
     func controller(_ controller: CreateTodoViewController, didCreateTodo todo: Todo) {
+        tableView.beginUpdates()
+        tableView.insertRows(at: [IndexPath(row: todoList.count, section: 0)],
+                             with: .automatic)
         todoList.append(todo)
+        tableView.endUpdates()
     }
 }
